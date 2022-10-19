69c1afb351800fa13dcbb77519081af0a4f4a294
Kelli Watkins <watkinskellil@gmail.com>
2022-10-19 16:20:54 -0600

    Try an array method

--- friends.js
+++ friends.js
@@ -2,3 +2,7 @@ var arrays = ["matt", "christa", "bryan", "kelly", "ann"]
 console.log(arrays[2]);
 console.log(arrays[4]);
 console.log(arrays[0]);
+
+var places = ["paris", "nyc", "canada", "italy", "utah"]
+places.shift();
+console.log(places)
